a3096458152df615444363c895d1000e78c00844
Alexander Malik <malik101112@mail.ru>
2023-11-29 02:50:59 +1000

    upload medic

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Medic
- Allows you to refill your health
+Allows you to refill your health
+A rewritten version of the [plugin](https://forums.alliedmods.net/showthread.php?p=886430) for the CS2 game on C#
+
+## Requirements
+- [Metamod](https://www.sourcemm.net/downloads.php/?branch=master)
+- [CounterStrikeSharp](https://github.com/roflmuffin/CounterStrikeSharp/releases/tag/v78) >= v78
+
+## Install
+- Move the contents of folder `build/` to `addons/counterstrikesharp/plugins/`
+- Configuration file will be generated after the plugin is launched in the folder `addons/counterstrikesharp/configs/plugins/medic`
+	
+### Config.json
+ - **MinHealth** - minimum HP, for the use of a medic
+ - **Healthhealth** - HP that will be restored after use
+ - **Cost** - the price of using a medic
+ - **ShowCall** - show everyone in the chat that the player used a medic [true / false]
+ - **MaxUse** - number of heal uses per round
+
+## About possible problems, please let me know: 
+[<img src="https://i.ibb.co/LJz83MH/a681b18dd681f38e599286a07a92225d.png" width="15.3%"/>](https://discordapp.com/users/858709381088935976/)
+[<img src="https://i.ibb.co/tJTTmxP/vk-process-mining.png" width="15.3%"/>](https://vk.com/bgtroll)
+[<img src="https://i.ibb.co/VjhryGb/png-transparent-brand-logo-steam-gump-s.png" width="15.3%"/>](https://hlmod.ru/members/palonez.92448/)
+[<img src="https://i.ibb.co/xHZPN0g/s-l500.png" width="15.3%"/>](https://steamcommunity.com/id/comecamecame)
+[<img src="https://i.ibb.co/S0LyzmX/tg-process-mining.png" width="16.3%"/>](https://t.me/ArrayListX)
+[<img src="https://i.ibb.co/Tb2gprD/2056021.png" width="15.3%"/>](https://github.com/Quake1011)
---
+++ src/Medic.cs
@@ -0,0 +1,85 @@
+﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes;
+using System.Text.Json.Serialization;
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Utils;
+
+namespace medic;
+
+public class ConfigGen : BasePluginConfig
+{
+    [JsonPropertyName("MinHealth")] public int MinHealth { get; set; } = 40;
+    [JsonPropertyName("HealHealth")] public int HealHealth { get; set; } = 100;
+    [JsonPropertyName("Cost")] public int Cost { get; set; } = 2000;
+    [JsonPropertyName("ShowCall")] public bool ShowCall { get; set; } = true;
+    [JsonPropertyName("MaxUse")] public int MaxUse { get; set; } = 1;
+}
+
+[MinimumApiVersion(78)]
+public class Medic : BasePlugin, IPluginConfig<ConfigGen>
+{
+    public override string ModuleName => "Medic";
+    public override string ModuleVersion => "0.0.2";
+    public override string ModuleAuthor => "Quake1011";
+    public ConfigGen Config { get; set; } = null!;
+    public void OnConfigParsed(ConfigGen config) { Config = config; }
+
+    private int[]? _tries;
+    
+    public override void Load(bool hotReload) { RegisterEventHandler<EventRoundStart>(OnRoundStart); }
+
+    [GameEventHandler(mode: HookMode.Post)]
+    private HookResult OnRoundStart(EventRoundStart @event, GameEventInfo info)
+    {
+        _tries ??= null;
+        _tries = Enumerable.Repeat(Config.MaxUse, Server.MaxPlayers).ToArray();
+        return HookResult.Continue;
+    }
+
+    [ConsoleCommand("heal", "Heal player")]
+    [ConsoleCommand("medic", "Heal player")]
+    [ConsoleCommand("doctor", "Heal player")]
+    [ConsoleCommand("hp", "Heal player")]
+    [CommandHelper(whoCanExecute: CommandUsage.CLIENT_ONLY)]
+    public void OnCommand(CCSPlayerController? activator, CommandInfo command)
+    {
+        if (activator == null) 
+			return;
+
+        if (_tries == null)
+            return;
+
+        if (_tries[activator.EntityIndex!.Value.Value - 1] <= 0)
+        {
+            activator.PrintToChat($" {ChatColors.Red}[Medic] {ChatColors.Default}The limit has been reached. Total: {ChatColors.Red}{Config.MaxUse}");
+            return;
+        }
+
+        if (activator.InGameMoneyServices!.Account < Config.Cost)
+        {
+            activator.PrintToChat($" {ChatColors.Red}[Medic] {ChatColors.Default}Not enough money to use medic. Need: {ChatColors.Red}{Config.Cost}$");
+            return;
+        }
+
+        if (activator.Health < Config.MinHealth)
+        {
+            activator.PrintToChat($" {ChatColors.Red}[Medic] {ChatColors.Default}Too little health for to use medic. Need: {ChatColors.Red}{Config.MinHealth}hp");
+            return;
+        }
+        
+        activator.InGameMoneyServices.Account -= Config.Cost;
+
+        var total = (activator.MaxHealth >= activator.Health + Config.HealHealth)
+            ? Config.HealHealth
+            : activator.MaxHealth - activator.Health;
+
+        activator.Health += total;
+                    
+        _tries[activator.EntityIndex!.Value.Value - 1]--;
+		
+        if (Config.ShowCall)
+            Server.PrintToChatAll($" {ChatColors.Red}[Medic] {ChatColors.Default}Player {ChatColors.Green}{activator.PlayerName}{ChatColors.Default} used medic and restore {ChatColors.Red}{total}hp");
+    }
+}
